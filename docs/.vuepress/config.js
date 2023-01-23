ca3b7a323656ea411681063abc13243fd7da62b3
Johnson Xin <2775145275@qq.com>
2023-01-23 23:49:02 +0800

    feat 添加导航栏

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports={
+    title:"JohnsonXin",
+    description:"JohnsonXin的个人博客",
+    themeConfig:{
+        // 作者
+        author:"JohnsonXin",
+        nav:[
+            {text:"首页",link:"/"},
+            {
+                text:"JohnsonXin 的小站",
+                items:[
+                    {
+                        text:"掘金",
+                        link:"https://juejin.cn/user/2573324506368663"
+                    },
+                    {
+                        text:"GitHub",
+                        link:"https://github.com/CodeGetters"
+                    }
+                ]
+            }
+        ],
+    }
+}
