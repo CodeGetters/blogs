a61a4e0336c106d37238bd57ee89eeb120512651
Johnson Xin <2775145275@qq.com>
2023-01-24 11:23:41 +0800

    style 音乐播放器

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -68,5 +68,47 @@ module.exports = {
                 zIndex: 999999999, // z-index property of the canvas, default: 999999999
             },
         ],
+        [
+            "@vuepress-reco/vuepress-plugin-bgm-player",
+            {
+                audios: [
+                    {
+                        name: "萤火之森",
+                        artist: "CMJ",
+                        url: "https://assets.smallsunnyfox.com/music/3.mp3",
+                        cover: "https://assets.smallsunnyfox.com/music/3.jpg"
+
+                    },
+                    {
+                        name: "我再没见过 想你一本的星空",
+                        artist: "Seto",
+                        url: "https://assets.smallsunnyfox.com/music/2.mp3",
+                        cover: "https://assets.smallsunnyfox.com/music/2.jpg"
+                    },
+                    {
+                        name: "Hear Me Now",
+                        artist: "Alok / Zeeba / Bruno Martini",
+                        //url: "https://sr-sycdn.kuwo.cn/2de791887191dd6da661970b37f98673/63cf4c20/resource/n2/26/68/3129151473.mp3",
+                        url: "Hear Me Now.mp3",
+                        cover: "http://p1.music.126.net/jB45ubgQ30qsGrv7MBkhEQ==/109951164852474437.jpg?param=130y130"
+                    },
+                    {
+                        name: "Diary of a poor kid",
+                        artist: "Papa Khan",
+                        //url: "https://cw-sycdn.kuwo.cn/fe435b5743a07f8ef4420b18fcd232e7/63cf4bc1/resource/n3/2/98/2383933166.mp3",
+                        url: "/Diary of a poor kid.mp3",
+                        cover: "http://p3.music.126.net/QmRNo3rHpNpt3VcRhd0BiA==/109951165406228694.jpg?param=300x300"
+                    },
+
+
+                ],
+                // 是否默认缩小
+                autoShrink: true,
+                // 缩小时缩为哪种模式
+                shrinkMode: "float",
+                // 悬浮窗样式
+                floatStyle: {bottom: "20px", "z-index": "999999"},
+            },
+        ],
     ]
 }
