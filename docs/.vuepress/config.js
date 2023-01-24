a9d3657dd2adb6e18d9a0a2d58f37d115a03effb
Johnson Xin <2775145275@qq.com>
2023-01-24 10:29:14 +0800

    style 鼠标点击效果

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,29 @@
-module.exports={
-    title:"JohnsonXin",
-    description:"JohnsonXin的个人博客",
-    theme:"reco",
-    locale:{
-        lang:"zh-CN",
+module.exports = {
+    title: "JohnsonXin",
+    description: "JohnsonXin的个人博客",
+    theme: "reco",
+    locale: {
+        lang: "zh-CN",
     },
-    themeConfig:{
-        type:"blog",
-        subSidebar:"auto",
-        logo:"/avatar.jpg",
-        authorAvatar:"/avatar.jpg",
+    themeConfig: {
+        type: "blog",
+        subSidebar: "auto",
+        logo: "/avatar.jpg",
+        authorAvatar: "/avatar.jpg",
         // 作者
-        author:"JohnsonXin",
-        nav:[
-            {text:"首页",link:"/"},
+        author: "JohnsonXin",
+        nav: [
+            {text: "首页", link: "/"},
             {
-                text:"JohnsonXin 的小站",
-                items:[
+                text: "JohnsonXin 的小站",
+                items: [
                     {
-                        text:"掘金",
-                        link:"https://juejin.cn/user/2573324506368663"
+                        text: "掘金",
+                        link: "https://juejin.cn/user/2573324506368663"
                     },
                     {
-                        text:"GitHub",
-                        link:"https://github.com/CodeGetters"
+                        text: "GitHub",
+                        link: "https://github.com/CodeGetters"
                     }
                 ]
             }
@@ -46,19 +46,27 @@ module.exports={
     },
     plugins: [
         [
-          "sakura",
-          {
-            // 默认数量
-            num: 20, 
-            //  是否显示
-            show: true, 
-            // 层级
-            zIndex: -1, 
-            img: {
-                // false 默认图 true 换图 需要填写httpUrl地址
-              replace: false, 
+            "sakura",
+            {
+                // 默认数量
+                num: 20,
+                //  是否显示
+                show: true,
+                // 层级
+                zIndex: -1,
+                img: {
+                    // false 默认图 true 换图 需要填写httpUrl地址
+                    replace: false,
+                },
             },
-          },
         ],
-     ]
+        [
+            "cursor-effects",
+            {
+                size: 4, // size of the particle, default: 2
+                shape: "star", // ['star' | 'circle'], // shape of the particle, default: 'star'
+                zIndex: 999999999, // z-index property of the canvas, default: 999999999
+            },
+        ],
+    ]
 }
