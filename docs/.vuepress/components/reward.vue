e78e42ac69ea7bd791f5921cd5f1ee7799c5e70c
Johnson Xin <2775145275@qq.com>
2023-02-18 10:16:29 +0800

    [feat]打赏

---
+++ docs/.vuepress/components/reward.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="donation">
+    <span>创作不易，赏杯奶茶钱吧~</span>
+    <br>
+    <br>
+    <button>打赏</button>
+    <div class="main">
+      <div class="pic">
+        <img src="https://txy.reday.asia/images/202302181014052.jpg" alt="微信">
+        <img src="https://txy.reday.asia/images/202302181014656.jpg" alt="支付宝">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "reward"
+}
+</script>
+
+<style scoped>
+
+</style>
