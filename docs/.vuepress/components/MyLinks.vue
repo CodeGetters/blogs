918d131632e29a4681c4913a1c504b043a34dab0
Johnson Xin <2775145275@qq.com>
2023-02-21 11:04:11 +0800

    update:blog

--- docs/.vuepress/components/MyLinks.vue
+++ docs/.vuepress/components/MyLinks.vue
@@ -19,13 +19,7 @@ const friendLinks = [
     desc: ' 一心一明月，一树一年华',
     avatar: 'https://www.pikamumu.top/logo.png',
     link: 'https://www.pikamumu.top',
-  },
-  {
-    title: 'ssw\'s Blog',
-    desc: '将进酒',
-    avatar: 'http://ssw.fit/free/vehicle.png',
-    link: 'http://ssw.fit',
-  },
+  }
 ]
 </script>
 
