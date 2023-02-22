17e8d634bf7228d515a820ac09ce9c9b8478095e
Johnson Xin <2775145275@qq.com>
2023-02-22 08:03:40 +0800

    feat:Algolia-Search

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,21 @@
+export default ({ router, Vue, isServer }) => {
+    Vue.mixin({
+        mounted() {
+            // 不加 setTimeout 会有报错，但不影响效果
+            setTimeout(() => {
+                try {
+                    docsearch({
+                        appId: "EWNGBGHKAL",
+                        apiKey: "e6f4888e459e829f835a89117159e5f0",
+                        indexName: "reday",
+                        container: '.search-box',
+                        // Set debug to true if you want to inspect the modal
+                        debug: false
+                    });
+                } catch(e) {
+                    console.log(e);
+                }
+            }, 100)
+        },
+    });
+};
