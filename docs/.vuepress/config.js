07062c67d78c8353bcd195ab5facda47a2df4f8a
Johnson Xin <2775145275@qq.com>
2023-02-27 21:44:36 +0800

    feat:插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -197,5 +197,60 @@ module.exports = {
         ['sitemap', {
             hostname: 'https://conimi.com'
         }],
+        //樱花飘落
+        ["sakura", {
+            // 默认数量
+            num: 20,
+            //  是否显示
+            show: true,
+            // 层级
+            zIndex: -1,
+            img: {
+                // false 默认图 true 换图 需要填写httpUrl地址
+                replace: false,
+            },
+        }],
+        //鼠标点击
+        ["cursor-effects", {
+            size: 4, // size of the particle, default: 2
+            shape: "star", // ['star' | 'circle'], // shape of the particle, default: 'star'
+            zIndex: 999999999, // z-index property of the canvas, default: 999999999
+        }],
+        //音乐播放器
+        ["@vuepress-reco/vuepress-plugin-bgm-player", {
+            audios: [
+                {
+                    name: "萤火之森",
+                    artist: "CMJ",
+                    url: "https://assets.smallsunnyfox.com/music/3.mp3",
+                    cover: "https://txy.reday.asia/images/202302161730108.jpg"
+
+                },
+                {
+                    name: "我再没见过 像你一般的星空",
+                    artist: "Seto",
+                    url: "https://assets.smallsunnyfox.com/music/2.mp3",
+                    cover: "https://assets.smallsunnyfox.com/music/2.jpg"
+                }],
+            // 是否默认缩小
+            autoShrink: false,
+            // 缩小时缩为哪种模式
+            shrinkMode: "float",
+            // 悬浮窗样式
+            floatStyle: {bottom: "20px", "z-index": "999999"},
+            //自动播放
+            autoplay: true
+        }],
+        //著作权信息
+        ['copyright', {
+            // 选中的文字将无法被复制
+            authorName: 'JohnsonXin',
+            // 如果长度超过  20 个字符
+            minLength: 20,
+        }],
+        //看板娘
+        ['@vuepress-reco/vuepress-plugin-kan-ban-niang', {
+            theme: ['blackCat', 'whiteCat', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'miku', 'z16']
+        }],
     ]
 }
